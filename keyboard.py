d9d9e588e6880472878d32c6f21ef9ea9a65b725
Gabriel Merlin <gabrielcmerlin@gmail.com>
2024-10-29 09:05:49 -0300

    Reviwed keyboard.py comments

--- keyboard.py
+++ keyboard.py
@@ -1,33 +1,43 @@
 # File used to handle keyboard events.
-# Most of the logic is from the code of our professor
+# Most of the logic is from the code of our professor Jean Roberto Ponciano.
 
 import math
 import glfw
 import glm
 
+# Handles polyMode changing using 'p' key.
 polyMode = False
-tree_scale = 1
-shrek_step = 0
-shrek_side_step = 0
-person_step = 0
-person_speed = 0
-cameraPos   = glm.vec3(0.0,  0.0,  1.0)
+
+# Handles camera movements.
+cameraPos = glm.vec3(0.0,  0.0,  1.0)
 cameraFront = glm.vec3(0.0,  0.0, -1.0)
-cameraUp    = glm.vec3(0.0,  1.0,  0.0)
-skyfix    = glm.vec3(0.0,  -72.0,  0.0)
+cameraUp = glm.vec3(0.0,  1.0,  0.0)
+skyfix = glm.vec3(0.0,  -72.0,  0.0) # used to make camera not surpass skybox sphere.
+
+# Handles mouse events.
 firstMouse = True
 yaw = -90.0 
 pitch = 0.0
-lastX =  700/2
-lastY =  700/2
+lastX = 700/2
+lastY = 700/2
 
-# Geometric transformations auxiliar variables.
+# Handles Shrek translation.
+shrek_step = 0
+shrek_side_step = 0
+
+# Handles rose scale.
 rose_scale_y = 0.05
+
+# Handles bird rotation.
 bird_speed = 0.00
 bird_radius = 80.0
 bird_angle = 0.0
 
 def key_event(window,key,scancode,action,mods):
+    '''
+    Handles inputs from the keyboard.
+    '''
+    
     global polyMode
     global cameraPos
     global cameraFront
@@ -39,12 +49,14 @@ def key_event(window,key,scancode,action,mods):
 
     cameraSpeed = 0.4
 
+    # Where Shrek can't go.
     exclusion_zones = [
         (-8, -30, 6, 1),
         (5, -9, 25, -2),             
         ]
     
-    if key == 87 and (action==1 or action==2): # tecla W
+    # Camera forward movent ('w' key).
+    if key == 87 and (action==1 or action==2):
         valid = True
         nova_pos = cameraPos + cameraSpeed * cameraFront
         if glm.length(nova_pos + skyfix) >= 96 or nova_pos[1] < -0.5:
@@ -55,7 +67,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             cameraPos = nova_pos
     
-    if key == 83 and (action==1 or action==2): # tecla S
+    # Camera backward movement ('s' key).
+    if key == 83 and (action==1 or action==2):
         valid = True
         nova_pos = cameraPos - cameraSpeed * cameraFront
         if glm.length(nova_pos +skyfix) >= 96 or nova_pos[1] < -0.5:
@@ -63,7 +76,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             cameraPos = nova_pos
     
-    if key == 65 and (action==1 or action==2): # tecla A
+    # Camera left movement ('a' key).
+    if key == 65 and (action==1 or action==2):
         valid = True
         nova_pos = cameraPos - glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
         if glm.length(nova_pos + skyfix) >= 96 or nova_pos[1] < -0.5:
@@ -71,7 +85,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             cameraPos = nova_pos
         
-    if key == 68 and (action==1 or action==2): # tecla D
+    # Camera right movement ('d' key).
+    if key == 68 and (action==1 or action==2):
         valid = True
         nova_pos = cameraPos + glm.normalize(glm.cross(cameraFront, cameraUp)) * cameraSpeed
         if glm.length(nova_pos +skyfix) >= 96 or nova_pos[1] < -0.5:
@@ -79,7 +94,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             cameraPos = nova_pos
 
-    if key == 73 and (action==1 or action==2): # tecla I
+    # Shrek forward movement ('i' key).
+    if key == 73 and (action==1 or action==2):
         valid = True
         for zone in exclusion_zones:
                 x_min, z_min, x_max, z_max = zone
@@ -90,7 +106,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             shrek_step -= 0.5 
         
-    if key == 75 and (action==1 or action==2): # tecla K
+    # Shrek backward movement ('k' key).
+    if key == 75 and (action==1 or action==2):
         valid = True
         for zone in exclusion_zones:
                 x_min, z_min, x_max, z_max = zone
@@ -101,7 +118,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             shrek_step += 0.5 
 
-    if key == 74 and (action==1 or action==2): # tecla J
+    # Shrek left movement ('j' key).
+    if key == 74 and (action==1 or action==2):
         valid = True
         for zone in exclusion_zones:
                 x_min, z_min, x_max, z_max = zone
@@ -112,7 +130,8 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             shrek_side_step -= 0.5 
 
-    if key == 76 and (action==1 or action==2): # tecla L
+    # Shrek right movement ('l' key).
+    if key == 76 and (action==1 or action==2):
         valid = True
         for zone in exclusion_zones:
                 x_min, z_min, x_max, z_max = zone
@@ -123,27 +142,37 @@ def key_event(window,key,scancode,action,mods):
         if valid:
             shrek_side_step += 0.5 
 
+    # Change visual mode (polymode).)
     if key == 80 and action == glfw.PRESS: 
         polyMode = not polyMode
 
-    if key == 265 and action == glfw.PRESS: # tecla seta pra cima (rosa cresce).
+    # Grows rose ('↑' key).
+    if key == 265 and action == glfw.PRESS:
         aux = rose_scale_y + 0.01
         rose_scale_y = min(aux, 0.13)
 
-    if key == 264 and action == glfw.PRESS: # tecla seta pra baixo (rosa diminui).
+    # Decreses rose ('↓' key).
+    if key == 264 and action == glfw.PRESS:
         aux = rose_scale_y - 0.01
         rose_scale_y = max(aux,0.05)
 
-    if key == 262 and action == glfw.PRESS: # tecla seta pra direita.
+    # Speed up bird ('→' key).
+    if key == 262 and action == glfw.PRESS:
         aux = bird_speed + 0.01
         bird_speed = min(aux, 0.05)
 
-    if key == 263 and action == glfw.PRESS: # tecla seta pra esquerda.
+    # Speed down bird ('←' key).
+    if key == 263 and action == glfw.PRESS:
         aux = bird_speed - 0.01
         bird_speed = max(aux, 0.0)
 
 def mouse_event(window, xpos, ypos):
+    '''
+    Handles inputs from the keyboard.
+    '''
+
     global firstMouse, cameraFront, yaw, pitch, lastX, lastY
+
     if firstMouse:
         lastX = xpos
         lastY = ypos
@@ -160,7 +189,6 @@ def mouse_event(window, xpos, ypos):
 
     yaw += xoffset
     pitch += yoffset
-
     
     if pitch >= 90.0: pitch = 90.0
     if pitch <= -90.0: pitch = -90.0
