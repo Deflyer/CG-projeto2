a972d23e8c77276bac85321947a03b51728fbf4a
Gabriel Merlin <gabrielcmerlin@gmail.com>
2024-10-29 09:19:05 -0300

    main.py comments reviewed

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ import glm
 window = init_window()
 program = create_program()
 
+# Configuring ambient variables.
 glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE)
 glEnable( GL_BLEND )
 glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA )
 glEnable(GL_LINE_SMOOTH)
 glEnable(GL_TEXTURE_2D)
-qtt_textures = 50
+qtt_textures = 12
 textures = glGenTextures(qtt_textures)
 
 # Getting all the vertexes and textures used in our project.
@@ -25,89 +26,51 @@ start = 0
 
 # Creating the shrek.
 shrek, coords_shrek, textures_shrek = get_vertexes_shrek()
-print(start)
-index_vertexes['shrek'] = [start]
-# We use a different method to read from an obj file, so different save
-for value in coords_shrek:
-    index_vertexes['shrek'].append(start + value)
-start = len(shrek) + start
+index_vertexes, start = create_object(index_vertexes, shrek, coords_shrek, 'shrek', start)
 
 # Creating the bathroom.
 bathroom, coords_bathroom, textures_bathroom = get_vertexes_bathroom()
-index_vertexes['bathroom'] = [start]
-for value in coords_bathroom:
-    index_vertexes['bathroom'].append(index_vertexes['bathroom'][-1] + value)
-start = len(bathroom) + start
+index_vertexes, start = create_object(index_vertexes, bathroom, coords_bathroom, 'bathroom', start)
 
 # Creating the house.
 house, coords_house, textures_house = get_vertexes_house()
-index_vertexes['house'] = [start]
-for value in coords_house:
-    index_vertexes['house'].append(index_vertexes['house'][-1] + value)
-start = len(house) + start
+index_vertexes, start = create_object(index_vertexes, house, coords_house, 'house', start)
 
 # Creating the sky.
 sky, coords_sky, textures_sky = get_vertexes_sky()
-index_vertexes['sky'] = [start]
-for value in coords_sky:
-    index_vertexes['sky'].append(index_vertexes['sky'][-1] + value)
-start = len(sky) + start
+index_vertexes, start = create_object(index_vertexes, sky, coords_sky, 'sky', start)
 
 # Creating the drawer.
 drawer, coords_drawer, textures_drawer = get_vertexes_drawer()
-index_vertexes['drawer'] = [start]
-for value in coords_drawer:
-    index_vertexes['drawer'].append(index_vertexes['drawer'][-1] + value)
-start = len(drawer) + start
+index_vertexes, start = create_object(index_vertexes, drawer, coords_drawer, 'drawer', start)
 
 # Creating the vase.
 vase, coords_vase, textures_vase = get_textures_vase()
-index_vertexes['vase'] = [start]
-for value in coords_vase:
-    index_vertexes['vase'].append(index_vertexes['vase'][-1] + value)
-start = len(vase) + start
+index_vertexes, start = create_object(index_vertexes, vase, coords_vase, 'vase', start)
 
 # Creating the rose.
 rose, coords_rose, textures_rose = get_textures_rose()
-index_vertexes['rose'] = [start]
-for value in coords_rose:
-    index_vertexes['rose'].append(index_vertexes['rose'][-1] + value)
-start = len(rose) + start
+index_vertexes, start = create_object(index_vertexes, rose, coords_rose, 'rose', start)
 
 # Creating the bed.
 bed, coords_bed, textures_bed = get_textures_bed()
-index_vertexes['bed'] = [start]
-for value in coords_bed:
-    index_vertexes['bed'].append(index_vertexes['bed'][-1] + value)
-start = len(bed) + start
+index_vertexes, start = create_object(index_vertexes, bed, coords_bed, 'bed', start)
 
 # Creating the ground.
 ground, coords_ground, textures_ground = get_textures_ground()
-index_vertexes['ground'] = [start]
-for value in coords_ground:
-    index_vertexes['ground'].append(index_vertexes['ground'][-1] + value)
-start = len(ground) + start
+index_vertexes, start = create_object(index_vertexes, ground, coords_ground, 'ground', start)
 
 # Creating the plant1.
 plant1, coords_plant1, textures_plant1, positions1 = get_vertexes_plant1()
-index_vertexes['plant1'] = [start]
-for value in coords_plant1:
-    index_vertexes['plant1'].append(index_vertexes['plant1'][-1] + value)
-start = len(plant1) + start
+index_vertexes, start = create_object(index_vertexes, plant1, coords_plant1, 'plant1', start)
 
 # Creating the plant2.
 plant2, coords_plant2, textures_plant2, positions2 = get_vertexes_plant2()
-index_vertexes['plant2'] = [start]
-for value in coords_plant2:
-    index_vertexes['plant2'].append(index_vertexes['plant2'][-1] + value)
-start = len(plant2) + start
+index_vertexes, start = create_object(index_vertexes, plant2, coords_plant2, 'plant2', start)
 
 # Creating the bird.
 bird, coords_bird, textures_bird = get_vertexes_bird()
-index_vertexes['bird'] = [start]
-for value in coords_bird:
-    index_vertexes['bird'].append(index_vertexes['bird'][-1] + value)
-start = len(bird) + start
+index_vertexes, start = create_object(index_vertexes, bird, coords_bird, 'bird', start)
 
 # Joining everyone
 vertexes_temp = np.concatenate((shrek, bathroom))
@@ -121,7 +84,6 @@ vertexes_temp = np.concatenate((vertexes_temp, ground))
 vertexes_temp = np.concatenate((vertexes_temp, plant1))
 vertexes_temp = np.concatenate((vertexes_temp, plant2))
 vertexes_temp = np.concatenate((vertexes_temp, bird))
-
 
 vertexes = np.zeros(len(vertexes_temp), [("position", np.float32, 3)])
 vertexes['position'] = vertexes_temp
@@ -143,7 +105,6 @@ glfw.set_cursor_pos_callback(window, kb.mouse_event)
 #-----------------------------------------------------------------------------------
 
 # Getting GPU variables.
-
 loc_color = get_loc_color(program)
 mat_view, loc_view = get_view(program)
 mat_projection, loc_projection = get_projection(program)
